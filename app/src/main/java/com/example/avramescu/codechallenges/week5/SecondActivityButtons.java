85a4063c0578b45a32a8fd53def4184b5dfda803
mihai-catalin.avramescu <mihai-catalin.avramescu@stefanini.com>
2019-11-29 17:29:18 +0200

    CodeChallenge2 Week5 - done

--- app/src/main/java/com/example/avramescu/codechallenges/week5/SecondActivityButtons.java
+++ app/src/main/java/com/example/avramescu/codechallenges/week5/SecondActivityButtons.java
@@ -5,32 +5,21 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.avramescu.codechallenges.R;
-import com.example.avramescu.codechallenges.week3.ScrollViewActivity;
 
 public class SecondActivityButtons extends AppCompatActivity {
 
-    private ScrollViewActivity mScrollView;
     private TextView mTextViewScrollview;
 
-    public static String textActivitate1;
-    public static String textActivitate2;
-    public static String textActivitate3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_buttons);
 
         initView();
+        getMessage();
     }
 
-    private void initView() {
-        mTextViewScrollview = findViewById(R.id.text_view_scroll_view);
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-
+    private void getMessage() {
         String message1 = null;
         String message2 = null;
         String message3 = null;
@@ -40,16 +29,19 @@ public class SecondActivityButtons extends AppCompatActivity {
             message2 = dataReceived.getString(FirstActivityButtons.CHEIEBUTON2);
             message3 = dataReceived.getString(FirstActivityButtons.CHEIEBUTON3);
 
-            if(message1 != null && message1.equals("mesaj1")){
-                mTextViewScrollview.setText(textActivitate1);
-            }
-            if(message2 != null && message2.equals("mesaj2")){
-                mTextViewScrollview.setText(textActivitate2);
-            }
-            if(message3 != null && message3.equals("mesaj3")){
-                mTextViewScrollview.setText(textActivitate3);
+            if(message1 != null){
+                mTextViewScrollview.setText(getResources().getString(R.string.pasaj1));
+            } else if(message2 != null){
+                mTextViewScrollview.setText(getResources().getString(R.string.pasaj2));
+            } else if(message3 != null){
+                mTextViewScrollview.setText(getResources().getString(R.string.pasaj3));
             }
         }
-
     }
+
+    private void initView() {
+        mTextViewScrollview = findViewById(R.id.text_view_scroll_view);
+    }
+
+
 }
