32f3faaca99c4cf2a7e9e2c17f222ef2dba16c70
Avramescu Mihai-Catalin <avramescumihaicatalin@gmail.com>
2020-03-05 22:47:18 +0200

    week6 - partially functional Tab Layout with View Pager

---
+++ app/src/main/java/com/example/avramescu/codechallenges/week6/tab/Tab2Fragment.java
@@ -0,0 +1,16 @@
+package com.example.avramescu.codechallenges.week6.tab;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.avramescu.codechallenges.R;
+
+public class Tab2Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
--- app/src/main/java/com/example/avramescu/codechallenges/week6/tab/TabActivity.java
+++ app/src/main/java/com/example/avramescu/codechallenges/week6/tab/TabActivity.java
@@ -4,27 +4,30 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.avramescu.codechallenges.R;
 
 public class TabActivity extends AppCompatActivity {
-
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
-
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        ViewPager viewPager = findViewById(R.id.view_pager);
-//        PagerAdapter
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "Tab 1");
+        adapter.addFragment(new Tab2Fragment(), "Tab 2");
+//        adapter.addFragment(new Tab3Fragment(), "Tab 3");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
