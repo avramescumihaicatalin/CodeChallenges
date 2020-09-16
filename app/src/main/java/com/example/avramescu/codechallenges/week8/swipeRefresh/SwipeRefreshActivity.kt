a0ecbd04d66f1256d8439aef58af19ac376d6630
mihai-catalin.avramescu <mihai-catalin.avramescu@stefanini.com>
2020-09-16 19:45:27 +0300

    Swipe Refresh Layout Activity added.

---
+++ app/src/main/java/com/example/avramescu/codechallenges/week8/swipeRefresh/SwipeRefreshActivity.kt
@@ -0,0 +1,42 @@
+package com.example.avramescu.codechallenges.week8.swipeRefresh
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.example.avramescu.codechallenges.R
+import kotlinx.android.synthetic.main.activity_swipe_refresh.*
+
+class SwipeRefreshActivity : AppCompatActivity() {
+
+    var counter: Int = 0
+//    var isLoading: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_swipe_refresh)
+
+        setSwipeListener()
+    }
+
+    private fun setSwipeListener() {
+        swipe_refresh_layout.setOnRefreshListener {
+//            isLoading = true
+            counter ++
+            Toast.makeText(this@SwipeRefreshActivity, "swiped: $counter times", Toast.LENGTH_SHORT).show()
+
+            val timer: CountDownTimer = object : CountDownTimer(3000, 1000) {
+                override fun onFinish() {
+                    swipe_refresh_layout.isRefreshing = false
+                }
+
+                override fun onTick(millisUntilFinished: Long) {
+                    //do nothing
+                }
+            }
+            timer.start()
+
+        }
+    }
+
+}
